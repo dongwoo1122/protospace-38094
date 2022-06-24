3084c3f7d3e261ea00d921b4778fafdd6c8347b4
dongwoo1122 <yoshikigota@gmail.com>
2022-06-24 19:26:06 +0900

    修正：ログアウトに関する条件分岐

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show ,:new, :create, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :update, :destroy]
 
   def index 
     @prototype = Prototype.all
@@ -52,7 +52,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
