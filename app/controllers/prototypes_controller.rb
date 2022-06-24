9cc8b478a6bddddcd3bc35f259dabcd1f1ead642
dongwoo1122 <yoshikigota@gmail.com>
2022-06-24 16:53:25 +0900

    修正（１）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show ,:new, :create, :update, :destroy]
 
   def index 
     @prototype = Prototype.all
